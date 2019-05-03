69e46d91aa73a0177155019433206d402985b9a6
rrwirya <rrwirya@ucdavis.edu>
2019-05-03 01:14:40 -0700

    Add files via upload

---
+++ Bplustree/LeafNode.h
@@ -0,0 +1,20 @@
+#ifndef LeafNodeH
+#define LeafNodeH
+
+#include "BTreeNode.h"
+
+class LeafNode:public BTreeNode
+{
+  int *values;
+public:
+  LeafNode(int LSize, InternalNode *p, BTreeNode *left,
+    BTreeNode *right);
+  LeafNode* Sort();	
+  int getMaximum() const; // ADDED
+  int getMinimum() const;
+  LeafNode* insert(int value); // returns pointer to new Leaf if splits
+  // else NULL
+  void print(Queue <BTreeNode*> &queue);
+}; //LeafNode class
+
+#endif
